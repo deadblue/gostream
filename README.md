d40613f5ef66cb6522e760ce67b056d0ff1c2fa9
deadblue <root@dead.blue>
2020-06-17 12:45:45 +0800

    Add README and doc.go.

---
+++ README.md
@@ -0,0 +1,5 @@
+# GoStream
+
+![](http://img.shields.io/badge/license-MIT-green.svg?style=flat-square)
+
+Stream utilities fo Go.
---
+++ doc.go
@@ -0,0 +1,3 @@
+// There is nothing on the top package.
+// Please check the sub packages for related features.
+package gostream
