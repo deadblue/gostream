a664cd984f535bb3dc9e909757991d779bc6e1e2
deadblue <root@dead.blue>
2020-06-17 19:49:43 +0800

    Add example code for chain.

---
+++ chain/example_test.go
@@ -0,0 +1,23 @@
+package chain
+
+import (
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+func Example() {
+	r1 := strings.NewReader("hello")
+	r2 := strings.NewReader(",")
+	r3 := strings.NewReader("world")
+	r4 := strings.NewReader("!")
+
+	r := JoinReader(r1, r2, r3, r4)
+	defer r.Close()
+
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Panicf("Read error: %s", err.Error())
+	}
+	log.Printf("Content: %s", string(data))
+}
