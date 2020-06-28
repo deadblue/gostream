8912190827716c30b1079b0f9475e0e6e94e78f6
deadblue <root@dead.blue>
2020-06-28 19:59:37 +0800

    Fire Observer.Done() only once.

--- observe/reader.go
+++ observe/reader.go
@@ -12,18 +12,23 @@ type reader struct {
 	flag *int32
 }
 
+func (r *reader) fireDone(err error) {
+	if atomic.CompareAndSwapInt32(r.flag, 0, 1) {
+		if err == nil || err == io.EOF {
+			r.o.Done(nil)
+		} else {
+			r.o.Done(err)
+		}
+	}
+}
+
 func (r *reader) Read(p []byte) (n int, err error) {
 	n, err = r.r.Read(p)
 	if n > 0 {
 		r.o.Transfer(n)
 	}
 	if err != nil {
-		if err == io.EOF {
-			r.o.Done(nil)
-		} else {
-			r.o.Done(err)
-		}
-		atomic.StoreInt32(r.flag, 1)
+		r.fireDone(err)
 	}
 	return
 }
@@ -32,10 +37,7 @@ func (r *reader) Close() (err error) {
 	if c, ok := r.r.(io.Closer); ok {
 		err = c.Close()
 	}
-	// Call Observer.Done() only once.
-	if atomic.CompareAndSwapInt32(r.flag, 0, 1) {
-		r.o.Done(err)
-	}
+	r.fireDone(err)
 	return
 }
 
